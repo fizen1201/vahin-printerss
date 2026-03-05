6344298abc636a0e3337b77189b9d0cb05acf9f3
Vishesh Koul <visheshkoul@gmail.com>
2026-03-05 16:12:50 +0530

    Updated map

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -15,5 +15,5 @@ export const contact: ContactInfo = {
     whatsapp: 'https://wa.me/919313040145',
   },
   mapEmbedUrl:
-    'https://maps.google.com/maps?q=3%2F2-A+B-Block+Patparganj+Industrial+Area+Jhilmil+Colony+New+Delhi+Delhi+110095&output=embed',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.5235633247553!2d77.3160755!3d28.673981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfb6e63e436f3%3A0x4867ce22998e8165!2sVahin%20Printers!5e0!3m2!1sen!2sin!4v1772706843211!5m2!1sen!2sin',
 }
